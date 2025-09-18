a6eb5abbacd34dedf89c2fb1459f91a0c0a2c0d0
root <root@LAPTOP-64VIDGDG.>
2025-09-18 17:25:49 -0500

    test 1

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Excel', 'SQL', 'Power BI', 'Python', 'R', 'WordPress'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,21 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
+              Greetings! I am Luis Arturo a Bachelor in Industrial engineer from the 
+              Universidad Peruana de Ciencias Aplicadas (UPC) focused on data analysis and project management 
+              based on the Project Management Institute (PMI) and the SCRUM framework.
               about HTML &amp; CSS!
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              I had the opportunity to work on different type of enterprises from startups, small, medium and big and 
+              in different type of industries like app development, telecommunications, consulting, minning and construction.           
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              I also have certifications like the Google Data Analytics Professional Certificate, 
+              SCRUM Foundation Professional Certificate, and SCRUM Master Professional Certificate. 
+                          
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
