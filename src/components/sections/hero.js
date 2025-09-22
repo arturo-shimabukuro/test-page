4e9ca66040842911f3a2e7a7540561e4677d5a5d
root <root@LAPTOP-64VIDGDG.>
2025-09-22 14:16:58 -0500

    test 1

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,17 +60,14 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Luis Arturo Shimabukuro Araki.</h2>;
+  const three = <h3 className="big-heading">I analyze and manage data and projects.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
+        I am Bachelor in Industrial engineer focus on data analysis and project management
+        to obatain insights and improve/create better processes. Always interested on new challenges and 
+        improving my skills at any opportunity I get.
         .
       </p>
     </>
@@ -78,10 +75,10 @@ const Hero = () => {
   const five = (
     <a
       className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
+      href=""
       target="_blank"
       rel="noreferrer">
-      Check out my course!
+      Hello there!
     </a>
   );
 
