c09c03bfd439406babf2d92b3c316f4eab7e746f
root <root@LAPTOP-64VIDGDG.>
2025-10-08 12:01:25 -0500

    socials change

--- src/config.js
+++ src/config.js
@@ -8,17 +8,10 @@ module.exports = {
     },
     
     {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
-    {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/luis-arturo-shimabukuro-araki/',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+    
   ],
 
   navLinks: [
