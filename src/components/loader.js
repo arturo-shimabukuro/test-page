50b364b9e3dd30a0aaa9be05582500950fbafc6f
root <root@LAPTOP-64VIDGDG.>
2025-10-08 22:41:08 -0500

    test new loader icon

--- src/components/loader.js
+++ src/components/loader.js
@@ -4,6 +4,7 @@ import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 import anime from 'animejs';
 import styled from 'styled-components';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const StyledLoader = styled.div`
   ${({ theme }) => theme.mixins.flexCenter};
