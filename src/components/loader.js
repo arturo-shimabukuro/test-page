cbf0e338a89f038eeacb4984175b3fd2a4088675
root <root@LAPTOP-64VIDGDG.>
2025-10-09 00:18:27 -0500

    test new loader icon

--- src/components/loader.js
+++ src/components/loader.js
@@ -19,18 +19,36 @@ const StyledLoader = styled.div`
   z-index: 99;
 
   .logo-wrapper {
+    position: relative;
     width: max-content;
     max-width: 150px;
     transition: var(--transition);
     opacity: ${props => (props.isMounted ? 1 : 0)};
     
+    .gatsby-image-wrapper,
     img {
       display: block;
       width: 100%;
       height: auto;
       margin: 0 auto;
       user-select: none;
-      filter: drop-shadow(0 0 20px rgba(100, 255, 218, 0.5));
+    }
+
+    svg {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      pointer-events: none;
+      
+      path {
+        fill: none;
+        stroke: var(--green);
+        stroke-width: 2;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+      }
     }
   }
 `;
@@ -44,22 +62,31 @@ const Loader = ({ finishLoading }) => {
     });
 
     loader
+      // Animate the hexagon stroke drawing
       .add({
-        targets: '.logo-wrapper .gatsby-image-wrapper',
+        targets: '#hex-outline',
         delay: 300,
         duration: 1500,
         easing: 'easeInOutQuart',
-        opacity: [0, 1],
-        scale: [0.8, 1],
+        strokeDashoffset: [anime.setDashoffset, 0],
       })
+      // Fade in the logo image
       .add({
         targets: '.logo-wrapper .gatsby-image-wrapper',
+        duration: 700,
+        easing: 'easeInOutQuart',
+        opacity: [0, 1],
+      }, '-=1000') // Start 1000ms before previous animation ends
+      // Scale out everything
+      .add({
+        targets: '.logo-wrapper',
         delay: 500,
         duration: 300,
         easing: 'easeInOutQuart',
         opacity: 0,
         scale: 0.1,
       })
+      // Hide loader
       .add({
         targets: '.loader',
         duration: 200,
@@ -81,25 +108,27 @@ const Loader = ({ finishLoading }) => {
 
       <div className="logo-wrapper">
         <StaticImage
-              className="img"
-              src="../images/as-logo.png"
-              quality={100}              
-              alt="logo"
-              backgroundColor="transparent"
-              placeholder="none"
-              style={{ opacity: 0 }}
-            />
-       <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+          className="img"
+          src="../images/as-logo.png"
+          quality={100}
+          alt="logo"
+          backgroundColor="transparent"
+          placeholder="none"
+          style={{ opacity: 0 }}
+        />
+        
+        {/* Animated hexagon overlay */}
+        <svg viewBox="0 0 120 110" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid meet">
           <path
             id="hex-outline"
-            d="M 50, 5
-               L 11, 27
-               L 11, 72
-               L 50, 95
-               L 89, 73
-               L 89, 28 z"
+            d="M 60, 8
+               L 15, 32
+               L 15, 80
+               L 60, 104
+               L 105, 80
+               L 105, 32 z"
           />
-        </svg> 
+        </svg>
       </div>
     </StyledLoader>
   );
