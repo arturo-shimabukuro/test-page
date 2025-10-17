f9c687aed9a4a99e1d714c7a3b3aa499f8de999d
arturo-shimabukuro <arturo-shimabukuro@users.noreply.github.com>
2025-10-17 22:03:04 +0000

    deploy: e2f421f5f3d59ab15f3f1643bca1b9483be80e21

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-460dc8415a1f433bb4a9.js"
+    "url": "webpack-runtime-f6ff4b27f14ccf81d1f2.js"
   },
   {
     "url": "framework-b2d6e508d75a672689cc.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "51bcdae3fec2cd5b16cf18fc3c2f850e"
+    "revision": "914755518f8b045cb33957dc0bde7aa5"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-e84596c37790733d100e.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "6116df8a06ec9db2ef4854e8d9967a8e"
+    "revision": "55280c0bb49a482b7f83376928fcb195"
   },
   {
     "url": "polyfill-34e6fb05e9bef66bd584.js"
